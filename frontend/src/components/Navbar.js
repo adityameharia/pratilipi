6bd5e797a918a0ace109571852c1e6fd2eb94102
Aditya Meharia <adityameharia14@gmail.com>
2022-02-08 10:50:19 +0530

    removed some files

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,16 +1,22 @@
-import {Box,Spacer,Text,Flex,Button} from '@chakra-ui/react'
-import {useState } from 'react';
-export default function Navbar({callbackTopCon}){
+import { Box, Spacer, Text, Flex, Button } from '@chakra-ui/react'
+import { useState } from 'react';
+export default function Navbar({ callbackTopCon }) {
     const [topcon, setTopcon] = useState(false);
-    return(
+    return (
         <Box bg="gray.100" p={2}>
             <Flex>
-            <Text marginLeft="5vw" fontSize="3xl" fontWeight="extrabold">Books</Text>
-            <Spacer/>
-            <Button marginRight="5vw" colorScheme="blue" variant={topcon?'solid':'outline'} onClick={()=>{
-                setTopcon(!topcon)
-                callbackTopCon(!topcon)
-            }}>Top contents</Button>
+                <Text
+                    marginLeft="5vw"
+                    fontSize="3xl"
+                    fontWeight="extrabold">Books</Text>
+                <Spacer />
+                <Button marginRight="5vw"
+                    colorScheme="blue"
+                    variant={topcon ? 'solid' : 'outline'}
+                    onClick={() => {
+                        setTopcon(!topcon)
+                        callbackTopCon(!topcon)
+                    }}>Top contents</Button>
             </Flex>
         </Box>
     )
